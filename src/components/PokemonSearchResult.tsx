0d254d78ed7bf5f3888669a5d97960013abf847f
Cedric98 <23346008+Cedric98@users.noreply.github.com>
2022-12-02 16:44:35 +0100

    Add pokemon types as badges in search

--- src/components/PokemonSearchResult.tsx
+++ src/components/PokemonSearchResult.tsx
@@ -1,15 +1,20 @@
 import { ReactElement } from 'react'
-import { Button, Card, Row } from 'react-bootstrap'
-import 'animate.css';
+import { Badge, Button, Card, Row } from 'react-bootstrap'
+import 'animate.css'
 import { Link } from 'react-router-dom'
-    
+
 // Will type this response in the future
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function PokemonSearchResult({ pokemon, cardHandleChange }: any): ReactElement {
-    if (pokemon.error) return <div>{ pokemon.error }</div>
+    if (pokemon.error) return <div>{pokemon.error}</div>
     if (Object.keys(pokemon).length === 0 || pokemon.name === undefined) return <div />
 
     const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const pokemonTypes = pokemon.types.map((element: any) => {
+        const pokemonTypeName = element.type.name.charAt(0).toUpperCase() + element.type.name.slice(1)
+        return <Badge className='me-1' key={pokemonTypeName}> {pokemonTypeName} </Badge>
+    })
 
     const nextPokemon = () => {
         cardHandleChange(pokemon.id + 1)
@@ -22,14 +27,13 @@ function PokemonSearchResult({ pokemon, cardHandleChange }: any): ReactElement {
         <Row>
             <div className='col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-12'>
                 <Card>
-                    <Card.Header>{ pokemonName } - #{ pokemon.id }</Card.Header>
+                    <Card.Header>{pokemonName} - #{pokemon.id}</Card.Header>
                     <Card.Img style={{ 'height': '30%', 'width': '100px' }} className='py-4 mx-auto d-block animate__animated animate__bounce' src={pokemon.sprites.other['official-artwork'].front_default} />
                     <Card.Body className='border-top'>
-                        <Card.Title></Card.Title>
-                        <Card.Text></Card.Text>
+                       {pokemonTypes}
                     </Card.Body>
                     <Row className='pb-3'>
-                        <Button className='col-md-1 offset-md-2' onClick={prevPokemon}>&laquo;</Button>  
+                        <Button className='col-md-1 offset-md-2' onClick={prevPokemon}>&laquo;</Button>
                         <Link className='col-md-4 offset-md-1' to={`/pokemon/${pokemon.name}`}>
                             <Button>More details</Button>
                         </Link>
